750607927c5354af813c12b88d5cb99787206624
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-19 15:23:11 +0100

    CIT-4 Add FermeController and extends from GenericControllerImpl

---
+++ src/main/java/org/youcode/citronix/controllers/web/FermeController.java
@@ -0,0 +1,17 @@
+package org.youcode.citronix.controllers.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.youcode.citronix.common.controllers.GenericControllerImpl;
+import org.youcode.citronix.dtos.request.FermeRequestDTO;
+import org.youcode.citronix.dtos.response.FermeResponseDTO;
+import org.youcode.citronix.entities.Ferme;
+import org.youcode.citronix.services.FermeService;
+
+@RestController
+@RequestMapping("/api/farmers")
+public class FermeController extends GenericControllerImpl<Ferme,Long, FermeRequestDTO, FermeResponseDTO> {
+    public FermeController(FermeService service) {
+        super(service);
+    }
+}
