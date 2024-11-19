5b58432a01bcceca0a00bfe29240b6be9cb8b8c2
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-19 10:50:39 +0100

    CIT-63 Add Champ entity

--- src/main/java/org/youcode/citronix/entities/Champ.java
+++ src/main/java/org/youcode/citronix/entities/Champ.java
@@ -1,27 +1,37 @@
 package org.youcode.citronix.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Accessors(chain = true)
 public class Champ {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String nom;
+    @NotNull
+    private double superficie;
 
-    @Column(nullable = false)
-    private String localisation;
+    @ManyToOne
+    @JoinColumn(name = "ferme_id", nullable = false)
+    private Ferme ferme;
 
-    @Column(nullable = false)
-    private double superficie; // en hectares
+    @OneToMany(mappedBy = "champ")
+    private List<Arbre> arbres = new ArrayList<>();
 
-    @Column(nullable = false)
-    private LocalDate dateCreation;
-
+    @OneToMany(mappedBy = "champ", cascade = CascadeType.ALL)
+    private List<Recolte> recoltes = new ArrayList<>();
 
 }
