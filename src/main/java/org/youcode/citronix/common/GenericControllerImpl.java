ea5f33ec12f06b7fd31972b2b5c3e761a9e1b15d
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-19 02:48:47 +0100

    CIT-1 feat: Add GenericControllerImpl

---
+++ src/main/java/org/youcode/citronix/common/GenericControllerImpl.java
@@ -0,0 +1,43 @@
+package org.youcode.citronix.common;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+public abstract class GenericControllerImpl<T, ID, RequestDto, ResponseDto> implements GenericController<ID, RequestDto, ResponseDto> {
+
+    protected GenericService<T, ID, RequestDto, ResponseDto> service;
+
+    public GenericControllerImpl(GenericService<T, ID, RequestDto, ResponseDto> service) {
+        this.service = service;
+    }
+
+    @Override
+    public ResponseEntity<ApiResponse<PagedResponse<ResponseDto>>> getAll(Pageable pageable) {
+        PagedResponse<ResponseDto> response = service.getAll(pageable);
+        return ResponseEntity.ok(ApiResponse.success(response, "Data retrieved successfully"));
+    }
+
+    @Override
+    public ResponseEntity<ApiResponse<ResponseDto>> getById(ID id) {
+        ResponseDto dto = service.getById(id);
+        return ResponseEntity.ok(ApiResponse.success(dto, "Item retrieved successfully"));
+    }
+
+    @Override
+    public ResponseEntity<ApiResponse<ResponseDto>> create(RequestDto requestDto) {
+        ResponseDto createdDto = service.create(requestDto);
+        return ResponseEntity.ok(ApiResponse.success(createdDto, "Item created successfully"));
+    }
+
+    @Override
+    public ResponseEntity<ApiResponse<ResponseDto>> update(ID id, RequestDto requestDto) {
+        ResponseDto updatedDto = service.update(id, requestDto);
+        return ResponseEntity.ok(ApiResponse.success(updatedDto, "Item updated successfully"));
+    }
+
+    @Override
+    public ResponseEntity<ApiResponse<Void>> delete(ID id) {
+        service.delete(id);
+        return ResponseEntity.ok(ApiResponse.success(null, "Item deleted successfully"));
+    }
+}
