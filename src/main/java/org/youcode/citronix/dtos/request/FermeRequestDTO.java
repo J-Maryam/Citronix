85f06825945df0e97fb8db1c8beb5f0c86ad93fd
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-19 15:03:08 +0100

    CIT-59 Add @UniqueValue annotation in FermeRequestDTO

--- src/main/java/org/youcode/citronix/dtos/request/FermeRequestDTO.java
+++ src/main/java/org/youcode/citronix/dtos/request/FermeRequestDTO.java
@@ -3,12 +3,14 @@ package org.youcode.citronix.dtos.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
+import org.youcode.citronix.common.validation.annotation.UniqueValue;
+import org.youcode.citronix.entities.Ferme;
 
 import java.time.LocalDate;
 
 public record FermeRequestDTO(
         @NotBlank
-        //add unique value annotation
+        @UniqueValue(entityClass = Ferme.class, fieldName = "nom", message = "Farmer name already exists")
         String nom,
 
         @NotBlank
