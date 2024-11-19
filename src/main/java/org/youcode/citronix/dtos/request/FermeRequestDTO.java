6696782aa17fdd59a2ef6f1fdfb9dc356be13c88
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-19 11:46:03 +0100

    CIT-63 Add FermeRequestDTO

---
+++ src/main/java/org/youcode/citronix/dtos/request/FermeRequestDTO.java
@@ -0,0 +1,26 @@
+package org.youcode.citronix.dtos.request;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
+public record FermeRequestDTO(
+        @NotBlank
+        //add unique value annotation
+        String nom,
+
+        @NotBlank
+        String localisation,
+
+        @NotNull
+        double superficie,
+
+        @NotNull
+        @PastOrPresent
+        LocalDate dateCreation
+
+) {
+}
