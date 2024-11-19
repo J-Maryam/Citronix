d88a01e788622b1d5f0c67740e28d38084158cda
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-19 14:56:41 +0100

    CIT-59 Add @Exists annotation on farm field in ChampRequestDTO

--- src/main/java/org/youcode/citronix/dtos/request/ChampRequestDTO.java
+++ src/main/java/org/youcode/citronix/dtos/request/ChampRequestDTO.java
@@ -1,12 +1,15 @@
 package org.youcode.citronix.dtos.request;
 
 import jakarta.validation.constraints.NotNull;
+import org.youcode.citronix.common.validation.annotation.Exists;
+import org.youcode.citronix.entities.Ferme;
 
 public record ChampRequestDTO(
         @NotNull
         double superficie,
 
         @NotNull
+        @Exists(entityClass = Ferme.class, message = "Farm id does not exists")
         Long ferme
 ) {
 }
