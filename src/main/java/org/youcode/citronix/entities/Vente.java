494c18c7f929c2715ac72bd88d26e2d0dcb0f86a
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-19 11:19:47 +0100

    CIT-63 Update Vente entity: add lambook annotation (@Data, @AllArgsConstructor ..)

--- src/main/java/org/youcode/citronix/entities/Vente.java
+++ src/main/java/org/youcode/citronix/entities/Vente.java
@@ -4,10 +4,18 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.time.LocalDate;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Accessors(chain = true)
 public class Vente {
 
     @Id
