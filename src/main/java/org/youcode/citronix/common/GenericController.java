5d5c49ef6246cba844e3fa792f92f34b006dab29
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-19 02:48:17 +0100

    CIT-1 feat: Add GenericController

---
+++ src/main/java/org/youcode/citronix/common/GenericController.java
@@ -0,0 +1,23 @@
+package org.youcode.citronix.common;
+
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public interface GenericController<ID, RequestDto, ResponseDto> {
+    @GetMapping
+    ResponseEntity<ApiResponse<PagedResponse<ResponseDto>>> getAll(Pageable pageable);
+
+    @GetMapping("/{id}")
+    ResponseEntity<ApiResponse<ResponseDto>> getById(@PathVariable ID id);
+
+    @PostMapping
+    ResponseEntity<ApiResponse<ResponseDto>> create(@RequestBody @Valid RequestDto requestDto);
+
+    @PutMapping("/{id}")
+    ResponseEntity<ApiResponse<ResponseDto>> update(@PathVariable ID id, @RequestBody @Valid RequestDto requestDto);
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<ApiResponse<Void>> delete(@PathVariable ID id);
+}
