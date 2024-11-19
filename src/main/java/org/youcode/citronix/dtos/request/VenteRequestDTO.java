28bda9ec852778d06b88892be04ef931988cfc35
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-19 15:05:25 +0100

    CIT-59 Add @Exists annotation in VenteRequestDTO

--- src/main/java/org/youcode/citronix/dtos/request/VenteRequestDTO.java
+++ src/main/java/org/youcode/citronix/dtos/request/VenteRequestDTO.java
@@ -3,6 +3,8 @@ package org.youcode.citronix.dtos.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
+import org.youcode.citronix.common.validation.annotation.Exists;
+import org.youcode.citronix.entities.Recolte;
 
 import java.time.LocalDate;
 
@@ -21,6 +23,7 @@ public record VenteRequestDTO(
         String client,
 
         @NotNull
+        @Exists(entityClass = Recolte.class, message = "Recolte id does not exists")
         Long recolte
 ) {
 }
