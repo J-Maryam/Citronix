5ed3481e38d7c548294947635efc4391f6811e1c
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-20 17:21:26 +0100

    create TreeController
    Add Controller and extends GenericControllerImpl to manage and implements Tree endpoints methods

--- src/main/java/org/youcode/citronix/controllers/web/TreeController.java
+++ src/main/java/org/youcode/citronix/controllers/web/TreeController.java
@@ -1,4 +1,18 @@
 package org.youcode.citronix.controllers.web;
 
-public class TreeController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.youcode.citronix.common.controllers.GenericControllerImpl;
+import org.youcode.citronix.dtos.request.TreeRequestDTO;
+import org.youcode.citronix.dtos.response.TreeResponseDTO;
+import org.youcode.citronix.entities.Tree;
+import org.youcode.citronix.services.TreeService;
+
+@RestController
+@RequestMapping("/api/trees")
+public class TreeController extends GenericControllerImpl<Tree, Long, TreeRequestDTO, TreeResponseDTO> {
+
+    public TreeController(TreeService service) {
+        super(service);
+    }
 }
