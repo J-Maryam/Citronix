c07b9215345a82f197f790b7c2e8e038bdbc552d
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-23 14:01:00 +0100

    CIT-8 feat: Implement create sale with validations and revenue calculation
    Retrieve the `Harvest` by its ID and throw an `EntityNotFoundException` if it does not exist.
    
    Check if the quantity requested is available, and throw an `IllegalArgumentException` if not.
    
    Update the quantity available in the harvest after a successful sale.
    
    Include the calculated revenue in the `SaleResponseDTO`.

--- src/main/java/org/youcode/citronix/services/Impl/SaleServiceImpl.java
+++ src/main/java/org/youcode/citronix/services/Impl/SaleServiceImpl.java
@@ -3,11 +3,14 @@ package org.youcode.citronix.services.Impl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
+import org.youcode.citronix.common.exceptions.EntityNotFoundException;
 import org.youcode.citronix.common.services.GenericServiceImpl;
 import org.youcode.citronix.dtos.request.SaleRequestDTO;
 import org.youcode.citronix.dtos.response.SaleResponseDTO;
+import org.youcode.citronix.entities.Harvest;
 import org.youcode.citronix.entities.Sale;
 import org.youcode.citronix.mappers.SaleMapper;
+import org.youcode.citronix.repositories.HarvestRepository;
 import org.youcode.citronix.repositories.SaleRepository;
 import org.youcode.citronix.services.SaleService;
 
@@ -16,7 +19,28 @@ import org.youcode.citronix.services.SaleService;
 @Transactional
 public class SaleServiceImpl extends GenericServiceImpl<Sale, Long, SaleRequestDTO, SaleResponseDTO> implements SaleService {
 
-    public SaleServiceImpl(SaleRepository repository, SaleMapper mapper) {
+    private final HarvestRepository harvestRepository;
+    public SaleServiceImpl(SaleRepository repository, SaleMapper mapper, HarvestRepository harvestRepository) {
         super(repository, mapper);
+        this.harvestRepository = harvestRepository;
+    }
+
+    @Override
+    public SaleResponseDTO create(SaleRequestDTO requestDto) {
+        Harvest existingHarvest = harvestRepository.findById(requestDto.harvestId())
+                .orElseThrow( () -> new EntityNotFoundException("Harvest with Id " + requestDto.harvestId() + " not found"));
+
+        if (requestDto.quantity() > existingHarvest.getTotalQuantity()) {
+            throw new IllegalArgumentException("The quantity demanded exceeds the quantity available in the harvest.");
+        }
+
+        existingHarvest.setTotalQuantity(existingHarvest.getTotalQuantity() - requestDto.quantity());
+        harvestRepository.save(existingHarvest);
+
+        Sale sale = mapper.toEntity(requestDto);
+        sale.setHarvest(existingHarvest);
+        sale.getRevenue();
+        Sale saved = repository.save(sale);
+        return mapper.toDto(saved);
     }
 }
